203c7948cb676ccc36e9a0ad13178ad7e18a6140
Jesusvigabriel <66917733+Jesusvigabriel@users.noreply.github.com>
2025-06-25 12:01:38 -0300

    Add remito retrieval by order

--- src/DALC/remitos.dalc.ts
+++ src/DALC/remitos.dalc.ts
@@ -11,3 +11,11 @@ export const remito_items_getByRemito_DALC = async (idRemito: number) => {
     const result = await getRepository(RemitoItem).find({ where: { IdRemito: idRemito }, relations: ["Orden"] });
     return result;
 };
+
+export const remito_getByOrden_DALC = async (idOrden: number) => {
+    const item = await getRepository(RemitoItem).findOne({
+        where: { IdOrden: idOrden },
+        relations: ["Remito", "Remito.Empresa", "Remito.PuntoVenta"],
+    });
+    return item ? item.Remito : null;
+};
